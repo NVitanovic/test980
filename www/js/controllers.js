5906dde3fc733aa0be346973296ce508352c47af
NVitanovic <x-team@live.com>
2014-11-04 00:08:13 +0100

    adeedadasd
    aaa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,9 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 	$scope.ime = "Vita";
+	//dodat niz
+	$scope.niz = ['ananas','jabuka','kruska','sljiva'];
+	$scope.niz.push('asdasd');
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
